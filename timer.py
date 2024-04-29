229868c247247b4fd4e154b877dc6e1ecc6c44aa
P4UL-M <paul.mairesse@free.fr>
2024-04-29 16:39:16 +0200

    NxGraph Subclass

--- timer.py
+++ timer.py
@@ -9,14 +9,14 @@ class staticproperty(staticmethod):
 # make a decorator to time the execution of a function and store it in a dictionary
 class Timer:
 
-    timedict = {}
+    timedict: dict[str, list] = {}
 
     @staticmethod
     def timeit(func):
         def wrapper(*args, **kwargs):
-            start = time.time()
+            start = time.perf_counter()
             result = func(*args, **kwargs)
-            end = time.time()
+            end = time.perf_counter()
             # if the function name is already in the dictionary, append the time to the list
             if func.__name__ in Timer.timedict:
                 Timer.timedict[func.__name__].append(end - start)
@@ -24,6 +24,35 @@ class Timer:
                 Timer.timedict[func.__name__] = [end - start]
             return result
         return wrapper
+
+    @staticmethod
+    def time(t1, name):
+        # add the difference between the two times to the dictionary
+        delta_time = time.perf_counter() - t1
+        if name in Timer.timedict:
+            Timer.timedict[name].append(delta_time)
+        else:
+            Timer.timedict[name] = [delta_time]
+
+    @staticmethod
+    def timeit_with_name(name):
+        def decorator(func):
+            def wrapper(*args, **kwargs):
+                start = time.perf_counter()
+                result = func(*args, **kwargs)
+                end = time.perf_counter()
+                # if the function name is already in the dictionary, append the time to the list
+                if name in Timer.timedict:
+                    Timer.timedict[name].append(end - start)
+                else:
+                    Timer.timedict[name] = [end - start]
+                return result
+            return wrapper
+        return decorator
+
+    @staticmethod
+    def get_time():
+        return time.perf_counter()
 
     @staticproperty
     def average_times():
